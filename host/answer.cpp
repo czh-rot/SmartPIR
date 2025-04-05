f64b6a2650e75e832c137c74f2696d8224e5a7ad
czh-rot <1244695706@qq.com>
2025-04-05 22:59:33 +0800

    Uploads Code

--- host/answer.cpp
+++
@@ -1,7 +0,0 @@
-#include "answer.hpp"
-//#include <define_host.h>
-
-unsigned long diff(const struct timeval *newTime, const struct timeval *oldTime)
-{
-    return (newTime->tv_sec - oldTime->tv_sec) * 1000000 + (newTime->tv_usec - oldTime->tv_usec);
-}
--- host/answer.hpp
+++
@@ -1,7 +0,0 @@
-#include "xcl2.hpp"
-#include "utils.hpp"
-#include "logger.hpp"
-#include <vector>
-
-//void estimated_cryptonets(std::string xclbin_path, unsigned long long *data_in0, unsigned long long *data_in1, unsigned long long *data_in2, unsigned long long *data_out0);
-void estimated_cryptonets(std::string xclbin_path,unsigned long long *ntt_out);
